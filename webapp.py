b5e9aa93ef0fe9b448e78c51e8242d61372cbe02
nathantrimble <97976030+nathantrimble@users.noreply.github.com>
2022-04-19 09:46:17 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -18,5 +18,16 @@ def startOver():
 def renderPage1():
     return render_template('page1.html')
 
+@app.route('/page2',methods=['GET','POST'])
+def renderPage2():
+    session["firstName"]=request.form['firstName']
+    session["lastName"]=request.form['lastName']
+    return render_template('page2.html')
+
+@app.route('/page3',methods=['GET','POST'])
+def renderPage3():
+    session["favoriteColor"]=request.form['favoriteColor']
+    return render_template('page3.html')
+
 if __name__=="__main__":
     app.run(debug=False)
